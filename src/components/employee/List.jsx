a012b39ca85ab7f73867174cd64d844a15fb18d2
Barsha Kalita <barshakalita02657@gmail.com>
2025-07-02 12:29:27 +0530

    image upload issue corrected

--- src/components/employee/List.jsx
+++ src/components/employee/List.jsx
@@ -28,7 +28,7 @@ const List=()=>{
                             dep_name: emp.department.dep_name,
                             name: emp.userId.name,
                             dob: new Date(emp.dob).toLocaleDateString(),
-                            profileImage: <img width={40} className='rounded-full' src={`https://employee-salary-leave-api-nn9s.vercel.app/${emp.userId.profileImage}`} />,
+                            profileImage: <img width={40} className='rounded-full' src={emp.userId.profileImage} />,
                             action: (<EmployeeButtons Id={emp._id} />),
                         }
                     ));
--- src/components/employee/View.jsx
+++ src/components/employee/View.jsx
@@ -37,7 +37,7 @@ const View = ()=>{
 
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
               <div>
-                <img  src={`https://employee-salary-leave-api-nn9s.vercel.app/${employee.userId.profileImage}`}
+                <img  src={employee.userId.profileImage}
                  className="rounded-full border w-72"
                 /> 
               </div>
